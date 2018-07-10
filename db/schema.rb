d6405f2aa7ac6916978b37a72de0160d27deec6c
Scott Goyette <sgoyette@kerberosfoundation.com>
2018-07-09 22:24:34 -0400

    Navigation added and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_185604) do
+ActiveRecord::Schema.define(version: 2018_07_10_022151) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_09_185604) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
